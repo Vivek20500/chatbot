360be9058600795f70a86b3bc7d17bba9e547a11
Vivek20500 <141113863+Vivek20500@users.noreply.github.com>
2025-07-06 00:42:10 +0530

    :(

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,9 @@ config();
 
 const app=express();
 
-app.use(cors({origin:`${process.env.FRONTEND_URL}`,credentials:true}));
+app.use(cors({origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
