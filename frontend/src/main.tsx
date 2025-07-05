46f85f9e99cb7b1b8810400cac97b1ddb74f6415
Vivek20500 <141113863+Vivek20500@users.noreply.github.com>
2025-07-05 23:00:56 +0530

    small updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/v1`;
 axios.defaults.withCredentials = true; // Enable sending cookies with requests
 
 const theme = createTheme({
