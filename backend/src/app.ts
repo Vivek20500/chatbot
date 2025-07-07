151a17c0ebf9589504e434b612712d701525f4b2
Vivek20500 <141113863+Vivek20500@users.noreply.github.com>
2025-07-07 14:34:34 +0530

    app.ts fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 config();
 
 const app=express();
-const allowedOrigins = [`${process.env.FRONTEND_URL}`];
+const allowedOrigins = process.env.FRONTEND_URL;
 
 app.use(cors({origin: allowedOrigins, 
   credentials: true,}));
